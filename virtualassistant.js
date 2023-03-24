695e14afa4d7c145d6967479794f881528d5d6b8
pooja-gera <55778515+pooja-gera@users.noreply.github.com>
2023-03-24 15:30:19 +0530

    initial commit for virtualassistant.js

---
+++ virtualassistant.js
@@ -0,0 +1,6 @@
+/*
+This serves as the project file for Objective 3: To use the OpenAI API to provide personalised responses and recommendations allowing for intelligent decision-making based on user preferences. 
+
+Last Updated: 24th March 2023
+*/
+
